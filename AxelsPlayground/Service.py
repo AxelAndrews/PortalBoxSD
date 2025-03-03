22a800acc672d30e80769baebe66f2b458696d45
Kevin Duong <kdd007@bucknell.edu>
2025-03-02 21:52:25 -0500

    added LCD Logic

--- AxelsPlayground/Service.py
+++ AxelsPlayground/Service.py
@@ -244,6 +244,11 @@ class PortalBoxApplication():
                 card_type_str = "Shutdown"
                 
             auth_str = "Auth" if new_input_data['user_is_authorized'] else "Unauth"
+            if new_input_data["user_is_authorized"]:
+                self.box.turn_LCD_Green()
+            else: 
+                self.box.turn_LCD_Red()
+        
             self.box.write_to_lcd(f"{card_type_str}-{auth_str}")
 
         # If no card is present, just update the button
@@ -255,6 +260,9 @@ class PortalBoxApplication():
                 "user_authority_level": 0,
                 "button_pressed": self.box.has_button_been_pressed()
             }
+            # Reset Button to Idle Blue
+            if new_input_data["button_pressed"]:
+                self.box.turn_LCD_Blue()
         # Else just use the old data and update the button
         # i.e., if there is a card, but it's the same as before
         else:
